85518d08d5219b90e62612deea01f1adace7bd7c
virjilakrum <asesenep15@gmail.com>
2024-09-25 18:26:33 +0300

    add sei-cli (seid)

---
+++ main/target/wasm32-unknown-unknown/release/libsei_zk_file_transfer.d
@@ -0,0 +1,1 @@
+/Users/dante/Desktop/reacthreejs/sei-zkp-file-transfer/main/target/wasm32-unknown-unknown/release/libsei_zk_file_transfer.rlib: /Users/dante/Desktop/reacthreejs/sei-zkp-file-transfer/main/src/lib.rs
---
+++ main/target/wasm32-unknown-unknown/release/sei_zk_file_transfer.d
@@ -0,0 +1,1 @@
+/Users/dante/Desktop/reacthreejs/sei-zkp-file-transfer/main/target/wasm32-unknown-unknown/release/sei_zk_file_transfer.wasm: /Users/dante/Desktop/reacthreejs/sei-zkp-file-transfer/main/src/lib.rs
