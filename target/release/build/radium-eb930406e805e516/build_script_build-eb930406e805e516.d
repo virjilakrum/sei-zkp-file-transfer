a44cd0645da7809a6e820241846afea46eef2956
virjilakrum <asesenep15@gmail.com>
2024-09-25 17:56:01 +0300

    add depends.

---
+++ target/release/build/radium-eb930406e805e516/build_script_build-eb930406e805e516.d
@@ -0,0 +1,5 @@
+/Users/dante/Desktop/reacthreejs/sei-zkp-file-transfer/target/release/build/radium-eb930406e805e516/build_script_build-eb930406e805e516: /Users/dante/.cargo/registry/src/index.crates.io-6f17d22bba15001f/radium-0.7.0/build.rs
+
+/Users/dante/Desktop/reacthreejs/sei-zkp-file-transfer/target/release/build/radium-eb930406e805e516/build_script_build-eb930406e805e516.d: /Users/dante/.cargo/registry/src/index.crates.io-6f17d22bba15001f/radium-0.7.0/build.rs
+
+/Users/dante/.cargo/registry/src/index.crates.io-6f17d22bba15001f/radium-0.7.0/build.rs:
